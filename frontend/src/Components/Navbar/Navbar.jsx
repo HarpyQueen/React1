61990485ea5b8b6f0f1ef069c18cbfec50f92d31
Binita Karki <binitakarki3355@gmail.com>
2024-03-27 18:17:04 +0545

    Added my project

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>Hello</p>
+            <img src={cart_icon} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
